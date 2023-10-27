887bb35051d0a992b25d3cbce079525db977070d
Harman Singh <singhharmandhindsa@gmail.com>
2023-10-27 20:35:00 +0530

    deployment configs

---
+++ pages/index.js
@@ -0,0 +1,26 @@
+import { Footer, Navbar } from "../components";
+import {
+  About,
+  Timeline,
+  Insights,
+  GetStarted,
+  Hero,
+  CardSection,
+  Objectives,
+} from "../sections";
+
+const Page = () => (
+  <div className="snap-y snap-mandatory overflow-y-scroll h-screen">
+    <Navbar />
+    <Hero />
+    <CardSection />
+    <About />
+    <Timeline />
+    <Insights />
+    <GetStarted />
+    <Objectives />
+    <Footer />
+  </div>
+);
+
+export default Page;
