4106659715c93c6f40dc31f74adb42185cd88223
Harman Singh <singhharmandhindsa@gmail.com>
2023-10-27 21:27:15 +0530

    solved minor bugs in deployment

--- next.config.js
+++ next.config.js
@@ -1,9 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: "export",
+  images: {
+    unoptimized:true
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
   experimental: {
     appDir: false,
   },
-  swcMinify:true
 };
 
 module.exports = nextConfig;
